e7f0c8c9eeafbd766660819337cdf977036324a6
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-11-28 18:59:26 +0530

    push

--- src/employees.py
+++ src/employees.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, Request
 from utils import get_conn
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter(tags=['Employees'])
+templates = Jinja2Templates(directory="templates")
 
 @router.get('/employees')
 def get_employees():
@@ -15,13 +17,56 @@ def get_employees():
     conn.close()
     return JSONResponse(content=rows)
 
-@router.get('/employees/{emp_id}')
-def get_employee_by_id(emp_id):
+@router.post('/employees')
+def get_employee_by_id(request: Request, emp_id = Form(...)):
     conn = get_conn()
     cur = conn.cursor()
     cur.execute('SELECT * FROM employees where employee_id=%s;', (emp_id,))
-    rows = cur.fetchone()
+    person = cur.fetchone()
     # conn.commit() only for write operations
     cur.close()
     conn.close()
-    return JSONResponse(content=rows)
+    
+    return templates.TemplateResponse('employee_details.html', {'request': request, 'person': person})
+
+@router.get('/employees/update/{emp_id}')
+def get_employee_update_form(request: Request, emp_id):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM employees where employee_id=%s;', (emp_id,))
+    person = cur.fetchone()
+    # conn.commit() only for write operations
+    cur.close()
+    conn.close()
+    
+    return templates.TemplateResponse('update_employee.html', {'request': request, 'person': person})
+
+
+@router.post('/employees/update')
+def get_employee_update_form(request: Request, emp_id = Form(...), emp_name= Form(...)):
+    conn = get_conn()
+    cur = conn.cursor()
+    print(emp_id, emp_name)
+    cur.execute('UPDATE employees SET name=%s where employee_id=%s;', (emp_name, emp_id.strip()))
+    conn.commit() 
+    cur.close()
+    conn.close()
+    
+    return templates.TemplateResponse('updated.html', {'request': request})
+
+
+@router.get('/employees/delete/{emp_id}')
+def get_employee_update_form(request: Request, emp_id):
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM employees where employee_id=%s;', (emp_id,))
+    conn.commit() 
+    cur.close()
+    conn.close()
+    
+    return templates.TemplateResponse('deleted.html', {'request': request})
+
+
+@router.get('/')
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {'request': request})
